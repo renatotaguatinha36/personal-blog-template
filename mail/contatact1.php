ad75db5935dab1e83048efee16d4769b91108ede
renatoguara2020 <renatoguara2020@gmail.com>
2024-03-25 11:06:05 -0300

    ok

---
+++ mail/contatact1.php
@@ -0,0 +1,36 @@
+require 'mailer/PHPMailerAutoload.php';
+
+<?php
+
+if (isset($_POST['assunto']) && !empty($_POST['assunto'])) {
+    $assunto = $_POST['assunto'];
+}
+if (isset($_POST['mensagem']) && !empty($_POST['mensagem'])) {
+    $mensagem = $_POST['mensagem'];
+}
+
+//$mail = new PHPMailer;
+
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls';
+$mail->Username = 'exemplo@gmail.com';
+$mail->Password = 'senha';
+$mail->Port = 587;
+
+$mail->setFrom('email@gmail.com', 'Contato');
+$mail->addAddress('email@mail.com.br');
+
+$mail->isHTML(true);
+
+$mail->Subject = $assunto;
+$mail->Body    = nl2br($mensagem);
+$mail->AltBody = nl2br(strip_tags($mensagem));
+
+if(!$mail->send()) {
+    echo 'Não foi possível enviar a mensagem.';
+    echo 'Erro: ' . $mail->ErrorInfo;
+} else {
+    header('Location: index.php?enviado');
+}
