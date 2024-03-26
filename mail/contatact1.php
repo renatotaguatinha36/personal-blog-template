4474d7d4b3bc5d50241b74e398fd0aeb87b1daa2
renatoguara2020 <renatoguara2020@gmail.com>
2024-03-26 11:48:26 -0300

    ok

--- mail/contatact1.php
+++ mail/contatact1.php
@@ -4,7 +4,7 @@ require 'mailer/PHPMailerAutoload.php';
 
 
 
-if (isset($_POST['assunto']) && !empty($_POST['assunto'])) {
+if(isset($_POST['assunto']) && !empty($_POST['assunto'])) {
     $assunto = $_POST['assunto'];
 }
 if (isset($_POST['mensagem']) && !empty($_POST['mensagem'])) {
