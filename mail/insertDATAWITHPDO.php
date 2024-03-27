4ebb99f36f64450330c017a44a2722e0d741d578
renatoguara2020 <renatoguara2020@gmail.com>
2024-03-27 17:56:10 -0300

    ok

--- mail/insertDATAWITHPDO.php
+++ mail/insertDATAWITHPDO.php
@@ -2,23 +2,20 @@
 $servername = "localhost";
 $username = "username";
 $password = "password";
-$dbname = "myDB";
+$dbname = "myDBPDO";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
-
-if ($conn->query($sql) === TRUE) {
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+  VALUES ('John', 'Doe', 'john@example.com')";
+  // use exec() because no results are returned
+  $conn->exec($sql);
   echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
 }
 
-$conn->close();
+$conn = null;
 ?>
