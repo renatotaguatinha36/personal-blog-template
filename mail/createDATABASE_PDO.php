62dd2dea2f33216c6fb799a1beb6b3e11da081f5
renatoguara2020 <renatoguara2020@gmail.com>
2024-03-27 17:41:50 -0300

    ok00

--- mail/createDATABASE_PDO.php
+++ mail/createDATABASE_PDO.php
@@ -13,9 +13,9 @@ try {
   // sql to create table
   $sql = "CREATE TABLE MyGuests (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  firstname VARCHAR(30) NOT NULL,
-  lastname VARCHAR(30) NOT NULL,
-  email VARCHAR(50),
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  email VARCHAR(250),
   reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
   )";
 
