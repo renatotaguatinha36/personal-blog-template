1f71adee7f21c80d90f1c30b9d5c61ac8a533a1b
renatoguara2020 <renatoguara2020@gmail.com>
2024-03-27 10:03:25 -0300

    ok1

--- CONNECTION_PDO.php
+++ CONNECTION_PDO.php
@@ -8,11 +8,12 @@ $id = $_GET['id'];
 try {
     $conn = new PDO('mysql:host=localhost;dbname=meuBancoDeDados', $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
 
     $stmt = $conn->prepare('SELECT * FROM minhaTabela WHERE id = :id');
     $stmt->execute(array('id' => $id));
 
-    while($row = $stmt->fetch()) {
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         print_r($row);
     }
 } catch(PDOException $e) {
